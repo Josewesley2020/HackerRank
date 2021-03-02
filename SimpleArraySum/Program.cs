17c0044f3dce96a2d73dc13e3580c921518fd915
Juliana <jwes.ofc1994@gmai.com>
2021-03-02 02:48:58 -0300

    Segundo Desafio

---
+++ SimpleArraySum/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Solution {
+
+    static int simpleArraySum(int[] ar) {
+        int soma = 0;
+        
+        for (int i = 0; i < ar.Length; i++){ 
+            
+            soma +=ar[i];
+        }
+         
+        return soma;
+    }
+
+    static void Main(string[] args) {
+       TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int arCount = Convert.ToInt32(Console.ReadLine());
+
+        int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp))
+        ;
+        int result = simpleArraySum(ar);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
