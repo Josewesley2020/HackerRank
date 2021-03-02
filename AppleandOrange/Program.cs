575940b8bee803d7f883f553adbb979e7a2a7e26
Juliana <jwes.ofc1994@gmai.com>
2021-03-02 03:41:42 -0300

    AppleandOrange

---
+++ AppleandOrange/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+    
+    static int fruits (int[] arr, int pos, int s, int t) {
+    
+         int cont = 0;
+         
+         for (int i = 0; i < arr.Count(); i++) {
+             if (arr[i] + pos >= s && arr[i] + pos <= t) {
+                 cont++;
+             }
+         } 
+         return cont;
+    }
+
+    // Complete the countApplesAndOranges function below.
+    static void countApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges) {
+        
+        
+        int apple = fruits(apples, a, s, t);
+        int orange = fruits(oranges, b, s, t);
+        
+        
+        Console.WriteLine(apple);
+        Console.WriteLine(orange);
+
+
+    }
+
+    static void Main(string[] args) {
+        string[] st = Console.ReadLine().Split(' ');
+
+        int s = Convert.ToInt32(st[0]);
+
+        int t = Convert.ToInt32(st[1]);
+
+        string[] ab = Console.ReadLine().Split(' ');
+
+        int a = Convert.ToInt32(ab[0]);
+
+        int b = Convert.ToInt32(ab[1]);
+
+        string[] mn = Console.ReadLine().Split(' ');
+
+        int m = Convert.ToInt32(mn[0]);
+
+        int n = Convert.ToInt32(mn[1]);
+
+        int[] apples = Array.ConvertAll(Console.ReadLine().Split(' '), applesTemp => Convert.ToInt32(applesTemp))
+        ;
+
+        int[] oranges = Array.ConvertAll(Console.ReadLine().Split(' '), orangesTemp => Convert.ToInt32(orangesTemp))
+        ;
+        countApplesAndOranges(s, t, a, b, apples, oranges);
+    }
+}
+
