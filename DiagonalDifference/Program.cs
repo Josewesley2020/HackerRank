8626d0e22a297e63dd0d775d9c841b5f2bf15d07
Juliana <jwes.ofc1994@gmai.com>
2021-03-02 03:07:33 -0300

    DiagonalDifference

---
+++ DiagonalDifference/Program.cs
@@ -0,0 +1,85 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'diagonalDifference' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int diagonalDifference(List<List<int>> arr)
+    {
+        int sumDiagonalEsq = 0;
+        int sumDiagonalDir = 0;
+        
+        for (int i = 0; i < arr.Count(); i++) {
+            
+            for (int j = 0; j < arr.Count(); j++) {
+                
+               if (i == j) {
+                   sumDiagonalEsq += arr[i][j];
+               }
+            }
+        }
+        
+        
+        for (int i = 0; i < arr.Count(); i++) {
+            
+            for (int j = 0; j < arr.Count(); j++) {
+                
+              if ( i + j == arr.Count() - 1 ) {
+                  
+                sumDiagonalDir += arr[i][j];
+                  
+              }
+              
+            }
+
+        }
+        
+        
+        return Math.Abs(sumDiagonalDir - sumDiagonalEsq); 
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<List<int>> arr = new List<List<int>>();
+
+        for (int i = 0; i < n; i++)
+        {
+            arr.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList());
+        }
+
+        int result = Result.diagonalDifference(arr);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
+
+
