42583e53468f2dcba9e3fec82ea39b5f3388df1d
Juliana <jwes.ofc1994@gmai.com>
2021-03-02 03:22:00 -0300

    Staircase$

---
+++ Staircase/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the staircase function below.
+    static void staircase(int n) {
+        string str = "#";
+        char vazio = ' ';
+        for (int i = 0; i < n; i++) {
+            Console.WriteLine(str.PadLeft(n, vazio));
+            str += "#";
+        }
+
+    }
+
+    static void Main(string[] args) {
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        staircase(n);
+    }
+}
